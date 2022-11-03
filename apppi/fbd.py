47224b0fdbad8af7dcafe6baba5f351302d8bb47
User <r121221@yandex.ru>
2022-11-03 13:19:25 +0300

    pi 03_11 создать таблицу для постов

---
+++ apppi/fbd.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+from apppi import app
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    ''' Вспомогательная функция по созданию БД '''
+    db = connect_db()
+    with app.open_resource('sql_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+    pass
+
+
+print(create_db.__doc__)
